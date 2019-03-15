b528790b660257f99a667106a759419145d5541e
Chien-Yu Lin <cyulin@linux15.ece.uw.edu>
2019-03-15 14:10:54 -0700

    finished Signoff-STA for the pparrot.

---
+++ ee477/ee477-designs/toplevels/bsg_black_parrot_be/tcl/hard/saed_90/macro_placement.tcl
@@ -0,0 +1,16 @@
+# TODO: Define where you want your macros here! Below is the main command that
+# you will be using:
+#     
+#    bsg_place_macro <hier name> <x location> <y location> <orientation = N,S,E,W>
+#
+
+bsg_place_macro be_mmu/dcache/genblk1_0__data_mem/macro_mem  100.000 997.950 E
+bsg_place_macro be_mmu/dcache/genblk1_1__data_mem/macro_mem  300.000 997.950 E
+bsg_place_macro be_mmu/dcache/genblk1_2__data_mem/macro_mem  500.000 997.950 E
+bsg_place_macro be_mmu/dcache/genblk1_3__data_mem/macro_mem  700.000 997.950 E
+bsg_place_macro be_mmu/dcache/genblk1_4__data_mem/macro_mem  900.000 997.950 E
+bsg_place_macro be_mmu/dcache/genblk1_5__data_mem/macro_mem 1100.000 997.950 E
+bsg_place_macro be_mmu/dcache/genblk1_6__data_mem/macro_mem 1300.000 997.950 E
+bsg_place_macro be_mmu/dcache/genblk1_7__data_mem/macro_mem 1500.000 997.950 E
+bsg_place_macro be_mmu/dcache/tag_mem/macro_mem 1841.395 133.630 E
+
